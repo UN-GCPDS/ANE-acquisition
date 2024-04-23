05f3799e9b5c6928eb08e113f4a30d18b1105a23
JCardenas05 <jocardenash@unal.edu.co>
2024-04-22 19:28:10 -0500

    add flask Dashboard

---
+++ flask-datta-able/apps/home/routes.py
@@ -0,0 +1,64 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.home import blueprint
+from flask import render_template, request
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+from flask import jsonify
+
+
+@blueprint.route('/index')
+@login_required
+def index():
+
+    return render_template('home/index.html', segment='index')
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
+
+@blueprint.route('/start_ppm', methods=['POST'])
+@login_required
+def start_bot():
+    data = request.json  # Accede a los datos JSON enviados en la solicitud
+    print(data)
+
+    response_message = f"started successfully."
+    return jsonify({'message': response_message})
