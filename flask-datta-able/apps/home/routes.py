cc437b8db53340ec690cbff1e1474741f94b92b5
Joseph Felipe Grijalva Lozano <114939614+Apricold@users.noreply.github.com>
2024-04-29 10:04:41 -0500

    se agrega fm stations css de dev-3 a dev-1 (#5)
    * add flask Dashboard
    
    * remove readme
    
    * se agrega codigo aparte con las funciones utilizadas y sdr-fm-scanner simplificado
    
    * se elimina archivos de texto
    
    * se hace migracion script de sdr_fm_scanner , waterfall y funciones , se integra los datos recibidos sobre las funciones de python , se reporta que no realiza el ciclo para calcular el psd de cada estacion de FM
    
    * json OK
    
    ---------
    
    Co-authored-by: JCardenas05 <jocardenash@unal.edu.co>

--- flask-datta-able/apps/home/routes.py
+++ flask-datta-able/apps/home/routes.py
@@ -71,6 +71,25 @@ def start_bot():
                 request.json[arg] = int(request.json[arg])
             elif arg == "threshold":
                 request.json[arg] = float(request.json[arg])
+<<<<<<< HEAD
+        print(request)
+
+        lista_frecuencias_encontradas=scan(request.json)
+
+    response_message = f"mensaje enviado."
+    return jsonify({'message': response_message})
+
+@blueprint.route('/test_cpu', methods=['GET'])
+def test_cpu():
+    start=time.time()
+    for i in range(1200):
+        a=i**2
+        time.sleep(0.05)
+        print(a)
+    print(f"el tiempo que se demora el codigo en correr es {time.time()-start}")
+    response_message = f"mensaje enviado."
+    return jsonify({'time': time.time()-start})
+=======
 
 #-------------------DEMODULACION EN FM DE LAS SEÑALES ENCONTRADAS Y WATERFALL -----------------------#
         lista_frecuencias_encontradas=scan(request.json,plot_waterfall=True)
@@ -92,16 +111,3 @@ def start_bot():
 
 
     return jsonify({'message': response_message})
-
-
-
-@blueprint.route('/test_cpu', methods=['GET'])
-def test_cpu():
-    start=time.time()
-    for i in range(1200):
-        a=i**2
-        time.sleep(0.05)
-        print(a)
-    print(f"el tiempo que se demora el codigo en correr es {time.time()-start}")
-    response_message = f"mensaje enviado."
-    return jsonify({'time': time.time()-start})
