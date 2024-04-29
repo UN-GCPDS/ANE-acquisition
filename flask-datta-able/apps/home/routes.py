9dccb2296f84b1987aa1e1f66504722dc2de356e
Joseph Felipe Grijalva Lozano <114939614+Apricold@users.noreply.github.com>
2024-04-29 10:31:00 -0500

    Cambios commit

--- flask-datta-able/apps/home/routes.py
+++ flask-datta-able/apps/home/routes.py
@@ -8,6 +8,8 @@ from apps.home.sdr_fm_scanner import scan
 from apps.home.funciones import fm_audio
 import json
 import time
+import os
+from apps.home.water_fall_class import Waterfall
 
 
 
@@ -62,20 +64,19 @@ def get_segment(request):
 @login_required
 def start_bot():
     if request.method == "POST":
+        start=time.time()
+        response_message = f"mensaje enviado."
         for arg in request.json:
             if arg !="city" and arg != "threshold":
                 request.json[arg] = int(request.json[arg])
             elif arg == "threshold":
                 request.json[arg] = float(request.json[arg])
-#-------------------DEMODULACION EN FM DE LAS SEÑALES ENCONTRADAS-----------------------#
-        lista_frecuencias_encontradas=scan(request.json,plot_waterfall=True)
-        for signal in lista_frecuencias_encontradas:
-            samples = fm_audio(fc=int(signal["freq"]), plot=True)
+        print(request)
+
+        lista_frecuencias_encontradas=scan(request.json)
 
     response_message = f"mensaje enviado."
     return jsonify({'message': response_message})
-
-
 
 @blueprint.route('/test_cpu', methods=['GET'])
 def test_cpu():
@@ -87,3 +88,24 @@ def test_cpu():
     print(f"el tiempo que se demora el codigo en correr es {time.time()-start}")
     response_message = f"mensaje enviado."
     return jsonify({'time': time.time()-start})
+
+#-------------------DEMODULACION EN FM DE LAS SEÑALES ENCONTRADAS Y WATERFALL -----------------------#
+        lista_frecuencias_encontradas=scan(request.json,plot_waterfall=True)
+        for signal in lista_frecuencias_encontradas:
+            newpath = f".\\apps\static\\assets\\images\\fm_stations\\{signal['freq'] / 1e6}"
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
+        #------------------------PLOTTING WATERFALL SECTION ---------------------------#
+            print(f"Band: {signal['freq'] / 1e6} MHz - PSD: {signal['psd']}")
+            wf = Waterfall()
+            wf.sdr.fc = signal["freq"]
+            print(wf.sdr.fc)
+            wf.showing_current_station(path=newpath)
+            #------------------------PLOTTING WATERFALL SECTION ---------------------------#
+            samples = fm_audio(fc=int(signal["freq"]), plot=True,path=newpath)
+        return jsonify({'time': time.time()-start})
+
+
+
+
+    return jsonify({'message': response_message})
