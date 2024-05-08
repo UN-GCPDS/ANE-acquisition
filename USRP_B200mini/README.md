c00abbd11bb079464999b6aa1e3847ff62d00040
bavendanom <bavendanom@unal.edu.co>
2024-05-08 18:22:09 -0500

    prueba del USRP

---
+++ USRP_B200mini/README.md
@@ -0,0 +1,3 @@
+https://pysdr.org/content/usrp.html  # USRP en Python.
+
+sudo apt install python3-uhd # Modulo de uhd
---
+++ USRP_B200mini/transmition/tx.py
@@ -0,0 +1,10 @@
+import uhd
+import numpy as np
+usrp = uhd.usrp.MultiUSRP("type=b200")
+samples = 0.8*np.random.randn(10000) + 0.8j*np.random.randn(10000) # create random signal
+duration = 60 # seconds
+center_freq = 88.300e6
+sample_rate = 200e3
+gain = 60 # [dB] start low then work your way up
+usrp.send_waveform(samples, duration, center_freq, sample_rate, [0], gain)
+# download uhd driver
