a23868a4218a5a2cb25f4930ced700bcd0655044
DANN9907 <dloaiza180@gmail.com>
2024-06-14 10:10:37 -0500

    Update scanningv1.py

--- CODIGO EN PYTHON/HackrfOne/scanningv1.py
+++ CODIGO EN PYTHON/HackrfOne/scanningv1.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from hackrf import HackRF
 from scipy import signal as sig
-from matplotlib.mlab import psd
+from gcpds.filters import frequency as flt
 
 class Scanner:
-    def __init__(self, start=88e6, stop=108e6, sample_rate=1e6, duration=5, num_rep=3):
+    def __init__(self, start=88e6, stop=108e6, sample_rate=10e6, duration=1, num_rep=1):
         self.start = start
         self.stop = stop
         self.sample_rate = sample_rate
@@ -17,17 +17,22 @@ class Scanner:
         num_freqs = int((self.stop - self.start) / self.sample_rate) + 1
         time_per_freq = (self.duration) / num_freqs
 
+        self.freq_vec = np.linspace(start, stop, num_freqs-1)
         self.samples_per_freq = int(self.sample_rate * time_per_freq)
 
         self.all_freqs = []
+        self.all_pwr = []
         self.all_psd = []
+        
 
     def scan(self):
         hackrf = HackRF()
-        hackrf.gain = '24'
+        hackrf.gain = 24
         hackrf.sample_rate = self.sample_rate
         
         freq = self.start + self.sample_rate / 2
+
+        print(f'Number of samples: {self.samples_per_freq}')
 
         t1 = time.time()
         
@@ -36,13 +41,18 @@ class Scanner:
                 print(f"Scanning frequency: {freq / 1e6} MHz")
                 hackrf.center_freq = freq
                 iq_samples = hackrf.read_samples(self.samples_per_freq)
+                iq_samples = iq_samples - np.mean(iq_samples)
 
-                b, a = sig.butter(4, 1000, 'highpass', fs=self.sample_rate)
-                iq_samples = sig.filtfilt(b, a, iq_samples)
+                high100 = flt.GenericButterHighPass(f0=0.01, N=1)
+                iq_samples = high100(iq_samples, fs=256)
 
-                freqs, psd = plt.psd(iq_samples, NFFT=1024, Fs=self.sample_rate / 1e6, Fc=hackrf.center_freq / 1e6)
+                avg_pwr = np.mean(np.abs(iq_samples)**2)
 
-                self.all_freqs.extend(freqs + (freq - self.sample_rate / 2) / 1e6)  # Ajuste de frecuencia
+                psd, freqs = plt.psd(iq_samples, NFFT=2048, Fs=self.sample_rate/1e6, Fc=hackrf.center_freq/1e6)
+                plt.close()
+
+                self.all_freqs.extend(freqs)  # Ajuste de frecuencia
+                self.all_pwr.append(avg_pwr)
                 self.all_psd.extend(psd)
 
                 freq += self.sample_rate
@@ -53,15 +63,37 @@ class Scanner:
 
     def plot_psd(self):
         plt.figure(figsize=(10, 6))
-        plt.plot(self.all_freqs, self.all_psd)
+        plt.plot(self.all_freqs, 10*np.log10(self.all_psd))
         plt.ylabel('PWR (dB)')
         plt.xlabel('Freq (MHz)')
         plt.title('Power Spectral Density')
         plt.grid(True)
         plt.show()
 
-hackrf = ScannerApp()
+    def plot_pwr(self):
+        print(self.all_pwr)
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.freq_vec, self.all_pwr)
+        plt.xlabel('Frequency (MHz)')
+        plt.ylabel('Average Power (W)')
+        plt.title('Average Power vs Frequency')
+        plt.grid(True)
+        plt.show()
+
+    def plt_Efield(self):
+        efield = np.sqrt(self.all_pwr)
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.freq_vec, efield)
+        plt.xlabel('Frequency (MHz)')
+        plt.ylabel('Average E field (V/m) ')
+        plt.title('Average E field vs Frequency')
+        plt.grid(True)
+        plt.show()
+
+hackrf = Scanner()
 
 hackrf.scan()
 hackrf.plot_psd()
+#hackrf.plot_pwr()
+#hackrf.plt_Efield()
 
